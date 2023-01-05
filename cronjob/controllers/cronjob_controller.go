b526ae91fce5cc8905fcb01a807d8ecd91b3c552
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-01-05 19:35:12 -0300

    get CronJob and list Jobs that belong to the CronJob

--- cronjob/controllers/cronjob_controller.go
+++ cronjob/controllers/cronjob_controller.go
@@ -19,23 +19,31 @@ package controllers
 import (
 	"context"
 
+	kbatch "k8s.io/api/batch/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	batchv1 "tutorial.kubebuilder.io/cronjob/api/v1"
+	batchv1 "tutorial.kubebuilder.io/project/api/v1"
+)
+
+const (
+	scheduledTimeAnnotation = "batch.tutorial.kubebuilder.io/scheduled-at"
 )
 
 // CronJobReconciler reconciles a CronJob object
 type CronJobReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+	Clock
 }
 
 //+kubebuilder:rbac:groups=batch.tutorial.kubebuilder.io,resources=cronjobs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=batch.tutorial.kubebuilder.io,resources=cronjobs/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=batch.tutorial.kubebuilder.io,resources=cronjobs/finalizers,verbs=update
+//+kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=batch,resources=jobs/status,verbs=get
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -47,7 +55,27 @@ type CronJobReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.1/pkg/reconcile
 func (r *CronJobReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
+
+	cronJob := batchv1.CronJob{}
+	if err := r.Get(ctx, req.NamespacedName, &cronJob); err != nil {
+		logger.Error(err, "unable to fetch CronJob")
+		// We'll ignore not-found errors, since they can't be fixed by an immediate
+		// requeue (we'll need to wait for a new notification), and we can get them
+		// on deleted requests.
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	childJobs := kbatch.JobList{}
+	if err := r.List(
+		ctx,
+		&childJobs,
+		client.InNamespace(req.Name),
+		client.MatchingFields{"jobOwnerKey": req.Name},
+	); err != nil {
+		logger.Error(err, "unable to list child jobs")
+		return ctrl.Result{}, err
+	}
 
 	// TODO(user): your logic here
 
